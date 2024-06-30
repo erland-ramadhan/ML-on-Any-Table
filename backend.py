dbe75ee34068b590c31ead90ab9c6489e08bf755
erland-ramadhan <48809651+erland-ramadhan@users.noreply.github.com>
2024-06-30 16:47:19 +0700

    Attempt #3

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 from gevent import monkey
+
 monkey.patch_all()
 
 from gevent import pywsgi
@@ -27,64 +28,70 @@ from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
 from skopt import BayesSearchCV
 from skopt.space import Real, Integer, Categorical
 from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    mean_absolute_percentage_error,
+)
 
 from tqdm import tqdm
 
-pd.set_option('future.no_silent_downcasting', True)
+pd.set_option("future.no_silent_downcasting", True)
 
-app = Flask(__name__, template_folder='frontend')
-socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
+app = Flask(__name__, template_folder="frontend")
+socketio = SocketIO(app, async_mode="gevent", cors_allowed_origins="*")
 
-UPLOAD_FOLDER = 'uploads'
-MODEL_FOLDER = 'models'
-FRONTEND_FOLDER = 'frontend'
-ALLOWED_EXTENSIONS = {'xlsx', 'csv', 'tsv'}
+UPLOAD_FOLDER = "uploads"
+MODEL_FOLDER = "models"
+FRONTEND_FOLDER = "frontend"
+ALLOWED_EXTENSIONS = {"xlsx", "csv", "tsv"}
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MODEL_FOLDER'] = MODEL_FOLDER
-app.config['FRONTEND_FOLDER'] = FRONTEND_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MODEL_FOLDER"] = MODEL_FOLDER
+app.config["FRONTEND_FOLDER"] = FRONTEND_FOLDER
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+    os.makedirs(app.config["UPLOAD_FOLDER"])
+
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def load_file(filepath):
     try:
-        if filepath.endswith('.csv'):
+        if filepath.endswith(".csv"):
             df = pd.read_csv(filepath)
-        elif filepath.endswith('.xlsx'):
+        elif filepath.endswith(".xlsx"):
             df = pd.read_excel(filepath)
-        elif filepath.endswith('.tsv'):
-            df = pd.read_csv(filepath, sep='\t')
+        elif filepath.endswith(".tsv"):
+            df = pd.read_csv(filepath, sep="\t")
         else:
-            raise ValueError('Unsupported file format')
+            raise ValueError("Unsupported file format")
 
         if df.empty:
-            raise ValueError('The uploaded file is empty.')
+            raise ValueError("The uploaded file is empty.")
 
         return df
     except pd.errors.EmptyDataError:
-        raise ValueError('No columns to parse from file')
+        raise ValueError("No columns to parse from file")
     except Exception as e:
-        raise ValueError(f'Error loading file: {e}')
+        raise ValueError(f"Error loading file: {e}")
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"})
 
-    file = request.files['file']
+    file = request.files["file"]
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+    if file.filename == "":
+        return jsonify({"error": "No selected file"})
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(filepath)
         try:
             df = load_file(filepath)
@@ -92,63 +99,66 @@ def upload_file():
 
             cramer_df = cramer_mat(df)
 
-            fig, ax = plt.subplots(figsize=(9,9))
-            sns.heatmap(cramer_df, annot=True, fmt='.2f')
+            fig, ax = plt.subplots(figsize=(9, 9))
+            sns.heatmap(cramer_df, annot=True, fmt=".2f")
             plt.title("Correlation Matrix between Columns of the Dataframe")
 
             buf = io.BytesIO()
-            plt.savefig(buf, format='png')
+            plt.savefig(buf, format="png")
             plt.close(fig)
             buf.seek(0)
 
-            cramer_image = base64.b64encode(buf.read()).decode('utf-8')
+            cramer_image = base64.b64encode(buf.read()).decode("utf-8")
             buf.close()
 
             json_outputs = {
-                    'dataframe_head': df_head,
-                    'filepath': filepath,
-                    'correlationHeatmap': cramer_image
-                    }
+                "dataframe_head": df_head,
+                "filepath": filepath,
+                "correlationHeatmap": cramer_image,
+            }
 
             return jsonify(json_outputs)
         except ValueError as e:
-            return jsonify({'error': str(e)})
+            return jsonify({"error": str(e)})
     else:
-        return jsonify({'error': 'Unsupported file format'})
+        return jsonify({"error": "Unsupported file format"})
+
 
 # Define a callable class for the callback
 class ProgressCallback:
     def __init__(self, total_iterations):
-        self.counter = {'iteration': 0}
-        self.pbar = tqdm(total=total_iterations, desc='Training Progress')
+        self.counter = {"iteration": 0}
+        self.pbar = tqdm(total=total_iterations, desc="Training Progress")
         self.total_iterations = total_iterations
 
     def __call__(self, res):
-        self.counter['iteration'] += 1
+        self.counter["iteration"] += 1
         self.pbar.update(1)
-        progress = self.counter['iteration'] / self.total_iterations * 100
-        socketio.emit('progress', {'progress': progress})
+        progress = self.counter["iteration"] / self.total_iterations * 100
+        socketio.emit("progress", {"progress": progress})
 
     def cleanup(self):
         self.pbar.close()
         self.pbar = None
 
+
 # Function to create the ProgressCallback instance
 def custom_callback(total_iterations):
     return ProgressCallback(total_iterations)
 
-@app.route('/train', methods=['POST'])
+
+@app.route("/train", methods=["POST"])
 def train_model():
     data = request.get_json()
-    filepath = data['filepath']
-    task_type = data['task_type']
-    target_column = data['target_column']
-    encoding_columns = data['encoding_columns']
+    filepath = data["filepath"]
+    task_type = data["task_type"]
+    target_column = data["target_column"]
+    encoding_columns = data["encoding_columns"]
 
     try:
         df = load_file(filepath)
     except ValueError as e:
-        return jsonify({'error': str(e)})
+        return jsonify({"error": str(e)})
 
     encoders = {}
 
@@ -157,10 +167,10 @@ def train_model():
     encoders[target_column] = target_encoder
 
     for column, enc_type in encoding_columns.items():
-        if enc_type == 'ordinal':
+        if enc_type == "ordinal":
             encoder = OrdinalEncoder()
             df[column] = encoder.fit_transform(df[column])
-        elif enc_type == 'loo':
+        elif enc_type == "loo":
             encoder = LeaveOneOutEncoder(cols=[column], sigma=0.1)
             df[column] = encoder.fit_transform(df[column], df[target_column])
         else:
@@ -173,32 +183,36 @@ def train_model():
     y = df[target_column]
 
     param_dist = {
-            'n_estimators': Integer(10, 200),
-            'max_depth': Integer(5, 20),
-            'min_samples_split': Integer(2, 11),
-            'min_samples_leaf': Integer(1, 11),
-            'max_features': Categorical(['sqrt', 'log2', None])
-            }
+        "n_estimators": Integer(10, 200),
+        "max_depth": Integer(5, 20),
+        "min_samples_split": Integer(2, 11),
+        "min_samples_leaf": Integer(1, 11),
+        "max_features": Categorical(["sqrt", "log2", None]),
+    }
 
-    if task_type == 'classification':
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+    if task_type == "classification":
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, stratify=y
+        )
         cv_ = StratifiedKFold(n_splits=5, shuffle=True)
-        scoring_ = 'f1'
-        model = RandomForestClassifier(bootstrap=True, class_weight='balanced')
+        scoring_ = "f1"
+        model = RandomForestClassifier(
+            bootstrap=True, class_weight="balanced", n_jobs=4
+        )
     else:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         cv_ = KFold(n_splits=5, shuffle=True)
-        scoring_ = 'neg_mean_squared_error'
-        model = RandomForestRegressor(bootstrap=True)
+        scoring_ = "neg_mean_squared_error"
+        model = RandomForestRegressor(bootstrap=True, n_jobs=4)
 
     bayes_search = BayesSearchCV(
-            estimator=model,
-            search_spaces=param_dist,
-            n_iter=50,
-            scoring=scoring_,
-            cv=cv_,
-            n_jobs=-1
-            )
+        estimator=model,
+        search_spaces=param_dist,
+        n_iter=50,
+        scoring=scoring_,
+        cv=cv_,
+        n_jobs=-1,
+    )
 
     callback_fn = custom_callback(total_iterations=50)
     bayes_search.fit(X_train, y_train, callback=callback_fn)
@@ -211,18 +225,20 @@ def train_model():
 
     y_eval = bayes_search.predict(X_test)
 
-    if task_type == 'classification':
+    if task_type == "classification":
         accuracy = accuracy_score(y_test, y_eval)
-        precision = precision_score(y_test, y_eval, average='weighted')
-        recall = recall_score(y_test, y_eval, average='weighted')
+        precision = precision_score(y_test, y_eval, average="weighted")
+        recall = recall_score(y_test, y_eval, average="weighted")
 
         accuracy_formatted = "{:.2f}%".format(accuracy * 100)
         precision_formatted = "{:.2f}%".format(precision * 100)
         recall_formatted = "{:.2f}%".format(recall * 100)
-        metrics = pd.DataFrame.from_dict({
-            'metrics': ['accuracy', 'precision', 'recall'],
-            'values': [accuracy_formatted, precision_formatted, recall_formatted]
-            })
+        metrics = pd.DataFrame.from_dict(
+            {
+                "metrics": ["accuracy", "precision", "recall"],
+                "values": [accuracy_formatted, precision_formatted, recall_formatted],
+            }
+        )
     else:
         mae = mean_absolute_error(y_test, y_eval)
         mape = mean_absolute_percentage_error(y_test, y_eval)
@@ -233,73 +249,82 @@ def train_model():
         mape_formatted = "{:.2f}%".format(mape)
         mse_formatted = "{:.2f}".format(mse)
         rmse_formatted = "{:.2f}".format(rmse)
-        metrics = pd.DataFrame.from_dict({
-            'metrics': ['mae', 'mape', 'mse', 'rmse'],
-            'values': [mae_formatted, mape_formatted, mse_formatted, rmse_formatted]
-            })
+        metrics = pd.DataFrame.from_dict(
+            {
+                "metrics": ["mae", "mape", "mse", "rmse"],
+                "values": [
+                    mae_formatted,
+                    mape_formatted,
+                    mse_formatted,
+                    rmse_formatted,
+                ],
+            }
+        )
 
     df_metrics = metrics.to_html()
 
     # Feature Importance Bar Plot
     feature_importances = bayes_search.best_estimator_.feature_importances_
     feature_names = X.columns
-    importance_df = pd.DataFrame({
-        'feature': feature_names,
-        'importance': feature_importances
-    }).sort_values(by='importance', ascending=False)
+    importance_df = pd.DataFrame(
+        {"feature": feature_names, "importance": feature_importances}
+    ).sort_values(by="importance", ascending=False)
 
     # Plotting with seaborn and using io.BytesIO to avoid file I/O
     fig, ax = plt.subplots(figsize=(15, 10))
-    sns.barplot(x='importance', y='feature', data=importance_df, ax=ax)
-    plt.title('Feature Importance')
-    plt.xlabel('Importance')
-    plt.ylabel('Feature')
+    sns.barplot(x="importance", y="feature", data=importance_df, ax=ax)
+    plt.title("Feature Importance")
+    plt.xlabel("Importance")
+    plt.ylabel("Feature")
 
     # Save plot to a bytes buffer
     buf = io.BytesIO()
-    plt.savefig(buf, format='png')
+    plt.savefig(buf, format="png")
     plt.close(fig)
     buf.seek(0)
 
     # Convert image to base64
-    importance_image = base64.b64encode(buf.read()).decode('utf-8')
+    importance_image = base64.b64encode(buf.read()).decode("utf-8")
     buf.close()
 
-    model_path = os.path.join(app.config['MODEL_FOLDER'], 'encoder_and_trained_rf.pkl')
+    model_path = os.path.join(app.config["MODEL_FOLDER"], "encoder_and_trained_rf.pkl")
 
-    with open(model_path, 'wb') as f:
-        joblib.dump((bayes_search, encoders), f, compress='lz4')
+    with open(model_path, "wb") as f:
+        joblib.dump((bayes_search, encoders), f, compress="lz4")
 
-    socketio.emit('train_complete', {'importance': importance_image, 'metrics': df_metrics})
+    socketio.emit(
+        "train_complete", {"importance": importance_image, "metrics": df_metrics}
+    )
 
     json_outputs = {
-            'message': 'Model trained successfully',
-            'model_path': model_path,
-            }
+        "message": "Model trained successfully",
+        "model_path": model_path,
+    }
 
     return jsonify(json_outputs)
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"})
 
-    file = request.files['file']
-    model_path = request.form['model_path']
-    target_column = request.form['target_column']
-    task_type = request.form['task_type']
+    file = request.files["file"]
+    model_path = request.form["model_path"]
+    target_column = request.form["target_column"]
+    task_type = request.form["task_type"]
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+    if file.filename == "":
+        return jsonify({"error": "No selected file"})
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         file.save(filepath)
         try:
             df = load_file(filepath)
         except ValueError as e:
-            return jsonify({'error': str(e)})
+            return jsonify({"error": str(e)})
 
         # Load the trained model and encoders
         model, encoders = joblib.load(model_path)
@@ -313,31 +338,36 @@ def predict():
         prediction = model.predict(df)
 
         # Decode the predictions if the target column is categorical
-        if task_type == 'classification' and target_column in encoders:
+        if task_type == "classification" and target_column in encoders:
             prediction = encoders[target_column].inverse_transform(prediction)
 
         # Since the input is a single row, return the single prediction value
-        prediction_value = prediction[0] if isinstance(prediction, (list, np.ndarray)) else prediction
+        prediction_value = (
+            prediction[0] if isinstance(prediction, (list, np.ndarray)) else prediction
+        )
         displayed_prediction = target_column + ": " + str(prediction_value)
 
-        return jsonify({'prediction': displayed_prediction})
+        return jsonify({"prediction": displayed_prediction})
 
-    return jsonify({'error': 'Unsupported file format'})
+    return jsonify({"error": "Unsupported file format"})
 
-@app.route('/frontend/<path:filename>')
+
+@app.route("/frontend/<path:filename>")
 def static_files(filename):
-    return send_from_directory(app.config['FRONTEND_FOLDER'], filename)
+    return send_from_directory(app.config["FRONTEND_FOLDER"], filename)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
 
-    if not os.path.exists(app.config['MODEL_FOLDER']):
-        os.makedirs(app.config['MODEL_FOLDER'])
+if __name__ == "__main__":
+    if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+        os.makedirs(app.config["UPLOAD_FOLDER"])
 
-    server = pywsgi.WSGIServer(('0.0.0.0', 8000), app, handler_class=WebSocketHandler)
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
+    if not os.path.exists(app.config["MODEL_FOLDER"]):
+        os.makedirs(app.config["MODEL_FOLDER"])
+
+    server = pywsgi.WSGIServer(("0.0.0.0", 8000), app, handler_class=WebSocketHandler)
+    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
